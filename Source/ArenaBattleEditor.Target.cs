eebff753936da10835861963a1460a2e5c76310a
4awol <skgustn3355@gmail.com>
2025-12-17 22:01:59 +0900

    GAS Start

--- Source/ArenaBattleEditor.Target.cs
+++ Source/ArenaBattleEditor.Target.cs
@@ -8,8 +8,7 @@ public class ArenaBattleEditorTarget : TargetRules
 	public ArenaBattleEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
         ExtraModuleNames.AddRange(new string[] { "ArenaBattle", "ArenaBattleGAS" });
     }
 }
